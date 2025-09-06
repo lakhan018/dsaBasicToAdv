b1b9557f8e31b5a9a5364e38eeef9a7c27b654f1
dushantpariska <dushantpariskar@gmail.com>
2025-09-06 17:20:52 +0530

    new changes

--- Basic/search/binary/BinaryS.cpp
+++ Basic/search/binary/BinaryS.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "BinaryS.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+int B(int arr[],int s,int e,int t) {
+ int mid = (s+e)/2;
+ if (arr[mid]==t) {
+  cout<<mid<<endl;
+ }
+ else if (arr[mid]>t) {
+  B(arr,s,mid,t);
+ }
+ else {
+  B(arr,mid+1,e,t);
+ }
+
+}
+int main() {
+ //Binary only needs structed data
+ int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+ int size = sizeof(arr) / sizeof(arr[0]);
+ int target=5;
+ cout<<B(arr,0,sizeof(arr[0]),target);
+
+}
